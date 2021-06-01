4ac1859f608acb0867bd27548b3caabf652e94ac
youtube-datavis <85189557+youtube-datavis@users.noreply.github.com>
2021-06-01 12:11:57 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Great Job!</h1>
+  <a href="https://www.youtube.com/channel/UCsZSBJ83B_xTW-L_eVgm2Xw" target="_blank">Your website is ready.</a>
+`;
